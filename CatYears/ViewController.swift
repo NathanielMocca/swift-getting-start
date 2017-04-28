c97c0e03d4e866f13c65644808858ddfb055bc73
Mocca Yang <tiffany12310@gmail.com>
2017-04-28 23:55:51 +0800

    cat years can count!

--- CatYears/ViewController.swift
+++ CatYears/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var catAgeLabel: UILabel!
+    
+    @IBAction func touchSubmitButton(_ sender: Any) {
+        let AgeOfCat = Int(ageField.text!)! * 7
+        catAgeLabel.text = String(AgeOfCat)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
